5e38f45f47e273bad890ebaa8f38e48b4c3a2497
Martin Lowe <martinlowe673@gmail.com>
2025-01-02 15:46:24 +0000

    prisma and relations

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
 }>) {
   return (
     
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
     <ClerkProvider>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
