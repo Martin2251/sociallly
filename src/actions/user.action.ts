4ecc65c51badf0fd087614a8061f79adf5a7140c
Martin Lowe <martinlowe673@gmail.com>
2025-01-04 12:30:45 +0000

    user action

--- src/actions/user.action.ts
+++ src/actions/user.action.ts
@@ -1,11 +1,23 @@
 "use server"
 
+import { prisma } from "@/lib/prisma";
 import { auth, currentUser } from "@clerk/nextjs/server"
 
 export async function syncUser(){
     try {
         const {userId} = await auth();
         const user = await currentUser();
+
+        if(!userId || !user) return ;
+        const dbUser = await prisma.user.create({
+            data:{
+                clerkId: userId,
+                name: `${user.firstName || ""} ${user.lastName || ""}`,
+                username: user.username ?? user.emailAddresses[0].emailAddress.split("@")[0],
+                email: user.emailAddresses[0].emailAddress,
+                image: user.imageUrl,
+            }
+        })
         
     } catch (error) {
         
